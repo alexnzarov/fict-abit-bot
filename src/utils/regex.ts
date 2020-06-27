8ca4cdb393cc5dffd69bcce7f994b40516b68505
Alexander Nazarov <alxkrekeris@gmail.com>
2020-06-27 21:56:51 +0300

    improved expressions

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,5 +1,5 @@
 export const any = (options: string[]) => (`(${options.join('|')})`);
 
 export const rotate = (a: string, b: string) => {
-  return `(${a}${b}|${b}|${a})`;
+  return `(${a}${b}|${b}${a})`;
 };
