b4bf89bbb77ca337e8154d666893366cfe3d87cb
Alexander <alxkrekeris@gmail.com>
2020-07-19 23:00:27 +0300

    added guide from @vosmiklasnizza

--- src/links.ts
+++ src/links.ts
@@ -64,6 +64,11 @@ const links: ILink[] = [
   */
   {
     goal: ResponseGoal.SPECIALTY,
+    title: 'Гайд для абитуры-программистов КПИ',
+    url: 'https://www.hashtap.com/@vosmiklasnizza/%D0%B3%D0%B0%D0%B9%D0%B4-%D0%B4%D0%BB%D1%8F-%D0%B0%D0%B1%D0%B8%D1%82%D1%83%D1%80%D1%8B-%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82%D0%BE%D0%B2-%D0%BA%D0%BF%D0%B8-v2-0-ZQM5oXbLWwYr',
+  },
+  {
+    goal: ResponseGoal.SPECIALTY,
     title: 'Откровенный разговор про 123 специальность',
     url: 'https://telegra.ph/V%D1%96dverta-rozmova-pro-123-na-F%D0%86OT%D1%96-04-11',
   },
